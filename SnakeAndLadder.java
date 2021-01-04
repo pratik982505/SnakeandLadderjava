54d9d7b44458088bd160c8d93d87b7afa7a3d32f
pratik982505 <gharatp%5@gmail.com>
2021-01-04 07:55:05 +0530

    DiceRoll

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,14 +1,23 @@
 package Game;
+
+import java.util.Random;
+
 public class SnakeAndLadder {
+	
 	static int position;
+	static int diceRoll;
 	public SnakeAndLadder() {
 		position = 0;
 		System.out.println("Welcome to Snake and Ladder Game");
 	}
-	
+	public void diceRoll() {
+		Random rand = new Random();
+		diceRoll = rand.nextInt(6)+1;
+		System.out.println("Dice Rolled :" + diceRoll);
+	}
 	public static void main(String[] args) {
 		SnakeAndLadder obj = new SnakeAndLadder();
 		System.out.println("Player at " + position + " position");
+		obj.diceRoll();
 	}
-
 }
